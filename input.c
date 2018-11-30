b1b342c9d9a7c4a10f42cb04593ca802c826e749
texruska <texruska@users.noreply.github.com>
2018-11-30 16:01:39 +0000

    Update input API and implementation

--- input.c
+++ input.c
@@ -2,14 +2,13 @@
 
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
 #define INPUT_MAX_NAME_LENGTH 256
 #define INPUT_MAX_NUM_FUNCS 10
 
 static struct input_func* registered_funcs[INPUT_MAX_NUM_FUNCS] = {NULL};
 static int init = 0;
-static int check_array_length(char arr[], size_t max_len);
+static void input_init(void);
 static void input_exit(void);
 static int is_init(void);
 static int input_get_func(char name[], struct input_func **rtn);
@@ -19,6 +18,12 @@ static struct input_func {
 	char name[INPUT_MAX_NAME_LENGTH];
 	int (*execute)(int argc, char *argv[]);
 };
+
+static void input_init(void) {
+	if (is_init()) { return; }
+	atexit(input_exit);
+	init = 1;
+}
 
 static void input_exit(void) {
 	for (int i=0; i < INPUT_MAX_NUM_FUNCS; i++) {
@@ -35,10 +40,23 @@ static int is_init(void) {
 	return init;
 }
 
-void input_init(void) {
-	if (is_init()) { return; }
-	atexit(input_exit);
-	init = 1;
+static int input_get_func(char name[], struct input_func **rtn) {
+	if (!is_init()) { input_init(); }
+
+	for (int i=0; i < INPUT_MAX_NUM_FUNCS; i++) {
+		struct input_func *f = registered_funcs[i];
+		if (f == NULL || f->name == NULL) { continue; }
+		if (strncmp(name,f->name,INPUT_MAX_NAME_LENGTH) == 0) { *rtn = f; return 1; }
+	}
+
+	return 0;
+}
+
+static struct input_func* input_allocate_struct(void) {
+	struct input_func *new_struct = malloc(sizeof(struct input_func));
+	strcpy(new_struct->name,"");
+	new_struct->execute = NULL;
+	return new_struct;
 }
 
 int input_register_func(char name[], int (*func)(int argc, char *argv[])) {
@@ -56,14 +74,17 @@ int input_register_func(char name[], int (*func)(int argc, char *argv[])) {
 	return 0;
 }
 
-static int input_get_func(char name[], struct input_func **rtn) {
-	if (!is_init()) { input_init(); }
+int input_deregister_func(char name[]) {
+	if (!is_init()) { return 0; }
 
 	for (int i=0; i < INPUT_MAX_NUM_FUNCS; i++) {
 		struct input_func *f = registered_funcs[i];
 		if (f == NULL || f->name == NULL) { continue; }
-		printf(f->name,"%s\n");
-		if (strncmp(name,f->name,INPUT_MAX_NAME_LENGTH) == 0) { *rtn = f; return 1; }
+		if (strncmp(name,f->name,INPUT_MAX_NAME_LENGTH) == 0) {
+			free(f);
+			registered_funcs[i] = NULL;
+			return 1;
+		}
 	}
 
 	return 0;
@@ -71,15 +92,12 @@ static int input_get_func(char name[], struct input_func **rtn) {
 
 int input_parse(int argc, char *argv[]) {
 	if (!is_init()) { input_init(); }
-	for (int i=0; i < argc; i++) {
-		printf("%s\n",argv[i]);
-	}
-	return 1;
-}
+	if (argc == 0) { return 0; }
 
-static struct input_func* input_allocate_struct(void) {
-	struct input_func *new_struct = malloc(sizeof(struct input_func));
-	strcpy(new_struct->name,"");
-	new_struct->execute = NULL;
-	return new_struct;
+	struct input_func *f;
+	if (input_get_func(argv[0],&f)) {
+		if (f->execute != NULL) { return f->execute(argc - 1, &argv[1]); }
+	}
+
+	return 0;
 }
--- input.h
+++ input.h
@@ -1,9 +1,9 @@
 #ifndef INPUT_H
 #define INPUT_H
 
-void input_init(void);
-
 int input_register_func(char name[], int (*func)(int argc, char *argv[]));
+
+int input_deregister_func(char name[]);
 
 int input_parse(int argc, char *argv[]);
 
