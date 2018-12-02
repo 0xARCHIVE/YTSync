fb684a6deae65479d435965fdc546b4722f9b1ac
texruska <texruska@users.noreply.github.com>
2018-12-02 22:52:05 +0000

    Update bookkeeper API

--- bookkeeper.h
+++ bookkeeper.h
@@ -3,24 +3,23 @@
 
 #include "ytdl.h"
 
-int bk_add(char name[], char url[], char dl_location[]);
+int bk_add(char * name, char * url, char * dl_location);
 
-int bk_list(int num_records, char *rtn[]);
+int bk_fetch_names(void);	// prepares bookkeeper to start fetching playlist names
+int bk_fetch_next_name(int num_chars, char * rtn);	// returns 0 when we're at the end of the records
 
-int bk_info(char name[], struct ytdl_playlist *rtn);
+int bk_info(char * name, struct ytdl_playlist * rtn);
 
-int bk_rename(char name[], char new_name[]);
+int bk_rename(char * name, char * new_name);
 
-int bk_mv(char name[], char dl_location[]);
+int bk_mv(char * name, char * dl_location);
 
-int bk_rm(char name[]);
+int bk_rm(char * name);
 
-int bk_cache_video(char name[], char yt_id[]);
+int bk_cache_video(char * name, char * yt_id);
 
-int bk_uncache_video(char name[], char yt_id[]);
+int bk_uncache_playlist(char * name);
 
-int bk_uncache_playlist(char name[]);
-
-int bk_is_cached(char name[], char yt_id[]);
+int bk_is_cached(char * name, char * yt_id);
 
 #endif
