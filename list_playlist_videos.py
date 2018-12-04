c8882ff5e8117a9d0e5db78df798d10ad9673283
texruska <texruska@users.noreply.github.com>
2018-12-04 17:11:50 +0000

    Add list_playlist_videos.py helper script

---
+++ list_playlist_videos.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import sys
+import youtube_dl
+
+def get_playlist_videos(playlist_id):
+	options = {
+		'extract_flat':True,
+		'simulate':True,
+		'quiet':True,
+		'ignoreerrors':True,
+	}
+
+	output = []
+
+	with youtube_dl.YoutubeDL(options) as ytdl:
+		try:
+			info = ytdl.extract_info("https://www.youtube.com/playlist?list=" + str(playlist_id))
+			[output.append(i['id']) for i in info['entries']]
+		except:
+			return "0"
+
+	return "\n".join(output)
+
+if __name__ == "__main__":
+	if (len(sys.argv) != 2):
+		sys.exit()
+	print(get_playlist_videos(sys.argv[1]))
